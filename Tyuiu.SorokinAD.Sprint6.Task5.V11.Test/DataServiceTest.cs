b66a48d58f9aeb6585a86b389f1f4bb00da22002
Tyuiu.SorokinAD@outlook.com <abuzius@mail.ru>
2023-12-10 21:50:08 +0500

    Task5

--- Tyuiu.SorokinAD.Sprint6.Task5.V11.Test/DataServiceTest.cs
+++ Tyuiu.SorokinAD.Sprint6.Task5.V11.Test/DataServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Tyuiu.SorokinAD.Sprint6.Task5.V11.Lib;
+using System.IO;
 
 namespace Tyuiu.SorokinAD.Sprint6.Task5.V11.Test
 {
@@ -8,8 +9,22 @@ namespace Tyuiu.SorokinAD.Sprint6.Task5.V11.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Valid()
         {
+            string path = @"C:\Users\blitz\source\repos\Tyuiu.SorokinAD.Sprint6\Tyuiu.SorokinAD.Sprint6.Task5.V11\bin\Debug\InPutFileTask5V11.txt";
+            DataService ds = new DataService();
+            double[] wait = new double[] { 10 }; 
+            double[] res = ds.LoadFromDataFile(path);
+            CollectionAssert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void LoadFromDataFile()
+        {
+            string path = @"C:\Users\blitz\source\repos\Tyuiu.SorokinAD.Sprint6\Tyuiu.SorokinAD.Sprint6.Task5.V11\bin\Debug\InPutFileTask5V11.txt";
+            FileInfo file = new FileInfo(path);
+            bool exists = file.Exists;
+            Assert.AreEqual(true, exists);
         }
     }
 }
